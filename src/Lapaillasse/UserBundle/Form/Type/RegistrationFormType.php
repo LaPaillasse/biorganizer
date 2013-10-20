a87be5ba99cbf86e81342e16d069c57c6137af1d
Pierre Mary <contact@pierre-mary.fr>
2013-10-21 01:03:03 +0200

    Database design updated
    Campaign and option tables added

---
+++ src/Lapaillasse/UserBundle/Form/Type/RegistrationFormType.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Lapaillasse\UserBundle\Form\Type;
+
+use Symfony\Component\Form\FormBuilderInterface;
+use FOS\UserBundle\Form\Type\RegistrationFormType as BaseType;
+
+class RegistrationFormType extends BaseType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+
+        // add your custom field
+        $builder->add('location');
+    }
+
+    public function getName()
+    {
+        return 'lapaillasse_user_registration';
+    }
+}
